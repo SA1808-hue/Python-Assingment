ad61f5797d5f468de287422bbebcc4ebcea77de2
Sandeep Gaddam <151390909+SA1808-hue@users.noreply.github.com>
2025-11-27 05:55:02 +0530

    Add files via upload

---
+++ Task 1. Assignment.py
@@ -0,0 +1,16 @@
+# Task1.Perform Basic Mathematical Operations
+
+num1 = 5
+num2 = 10
+
+# Addition
+print(num1 + num2)
+
+# Subtraction
+print(num1 - num2)
+
+# Multiplication
+print(num1 * num2)
+
+# Division
+print(num1 / num2)
---
+++ Task2 .Assignment.py
@@ -0,0 +1,18 @@
+# Task 2: Create a Personalized Greeting
+
+# Take user input
+first_name =input("Enter your first name:")
+
+last_name = input("Enter your last name:")
+
+# Concatenate full name
+full_name = first_name + " " + last_name
+
+# Print greeting message
+print("Hello,","Sandeep","Gaddam""!","Welcome to the Python program"".")
+
+
+
+
+
+
