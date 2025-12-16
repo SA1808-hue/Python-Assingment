b539cf4a3c5876ffd3f82b701fc9ad227ae06166
Sandeep Gaddam <151390909+SA1808-hue@users.noreply.github.com>
2025-12-16 10:44:32 +0530

    Update greeting message to use user input

--- Task2 .Assignment.py
+++ Task2 .Assignment.py
@@ -1,15 +1,17 @@
 # Task 2: Create a Personalized Greeting
 
 # Take user input
-first_name =input("Enter your first name:")
-
-last_name = input("Enter your last name:")
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
 
 # Concatenate full name
 full_name = first_name + " " + last_name
 
-# Print greeting message
-print("Hello,","Sandeep","Gaddam""!","Welcome to the Python program"".")
+# Print greeting message using input values
+print("Hello, " + full_name + "! Welcome to the Python program.")
+
+
+
 
 
 
