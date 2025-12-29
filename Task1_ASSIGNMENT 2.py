f18e1476b73c2e9ba3323ac0de470279401ce28f
Sandeep Gaddam <151390909+SA1808-hue@users.noreply.github.com>
2025-12-29 23:28:17 +0530

    Add files via upload

---
+++ Task1_ASSIGNMENT 2.py
@@ -0,0 +1,8 @@
+# Take integer input from the user
+number = int(input("Enter an integer number: "))
+
+# Check whether the number is even or odd
+if number % 2 == 0:
+    print(f"{number} is an Even number.")
+else:
+    print(f"{number} is an Odd number.")
---
+++ Task2_ASSIGNMENT 2.py
@@ -0,0 +1,9 @@
+# Initialize sum variable
+total_sum = 0
+
+# Loop from 1 to 50
+for i in range(1, 51):
+    total_sum += i
+
+# Display the result
+print("The sum of integers from 1 to 50 is:", total_sum)
